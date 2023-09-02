6c0b857a60c439c03181e90aa0a597bd310f7588
Kaue Henrique Domingues <106930919+Kaued@users.noreply.github.com>
2023-09-02 14:16:01 -0300

    create project

---
+++ Migrations/20230901171704_PopularCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Value('Bebidas', 'bebidas.png')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Value('Lanches', 'lanches.png')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Value('Teste', 'teste.png')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
